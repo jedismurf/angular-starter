2a7f23ccd7b33e8e6aeb3e20ceb012685e931de0
Frank Altamirano <smurfy@bigpond.net.au>
2017-02-10 12:27:24 +1100

    Set up angular configuration

---
+++ app/app.js
@@ -0,0 +1,22 @@
+'use strict';
+
+angular.module('myApp', [
+    'ngRoute',
+    'myApp.application'
+])
+
+.config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider){
+    
+    // LOCATION SERVICE CONFIGURATION
+    $locationProvider.hashPrefix('!');
+    $locationProvider.html5Mode({
+        enabled: true,
+        requireBase: true,
+        rewriteLinks: true
+    });
+
+    // ROUTER SERVICE CONFIGURATION
+    $routeProvider
+        .when('/', { template: '<application></application>' })
+        .otherwise({redirectTo: '/'});
+}])
